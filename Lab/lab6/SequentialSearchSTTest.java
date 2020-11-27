02a9f1984e20c89822cd7e3252e2069e1c40a746
Sonamdema123 <sdema1684@gmail.com>
2020-11-27 22:13:46 +0600

    adding file

--- Lab/lab6/SequentialSearchSTTest.java
+++ Lab/lab6/SequentialSearchSTTest.java
@@ -2,16 +2,16 @@ public class SequentialSearchSTTest{
 	public static void main(String[] args) {
 		SequentialSearchST<String, Integer> obj = new SequentialSearchST<String, Integer>();
 
-		obj.put("A",5);
-		obj.put("B",7);
-		obj.put(null,6);
+		// obj.put("A",5);
+		// obj.put("B",7);
+		// obj.put(null,6);
 
-		obj.delete("B");
+		//obj.delete("B");
 
 		System.out.println(obj.size());
 		System.out.println(obj.isEmpty());
 		System.out.println(obj.contains("A"));
 		System.out.println(obj.get("A"));
-		System.out.println(obj.get("B"));
+		//System.out.println(obj.get("B"));
 	}
 }
